b5d750f0f81f9e7e851e24b8d5fc43a233a0dfa5
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-11-03 21:33:46 -0500

    Update solana.ts

--- src/solana.ts
+++ src/solana.ts
@@ -1,16 +1,21 @@
 import { Blockchain } from "./blockchain";
 import { Connection, PublicKey } from "@solana/web3.js";
 
-export class Solana implements Blockchain{
-    private connection: Connection;
+export class Solana implements Blockchain {
+  private connection: Connection;
 
-    constructor(rpcUrl: string){
-        this.connection = new Connection(rpcUrl);
+  constructor(rpcUrl: string) {
+    this.connection = new Connection(rpcUrl);
+  }
+
+  async getBalance(address: string): Promise<string> {
+    try {
+      const publicKey = new PublicKey(address);
+      const balance = await this.connection.getBalance(publicKey);
+      return (balance / 1e9).toFixed(4); 
+    } catch (error) {
+      console.error("Error fetching Solana balance:", error);
+      throw new Error("Error fetching Solana balance");
     }
-    
-    async getBalance(address: string): Promise<string> {
-        const publicKey = new PublicKey(address);
-        const balance = await this.connection.getBalance(publicKey);
-        return (balance / 1e9).toString();
-    }
+  }
 }
